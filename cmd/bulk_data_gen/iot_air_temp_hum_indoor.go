bfaebc02cbcf0cdfb9ae28ca448ead129481b2b7
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-06-07 13:43:05 +0200

    Added IoT indoor air condition measurements

---
+++ cmd/bulk_data_gen/iot_air_temp_hum_indoor.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"time"
+)
+
+var (
+	ATHIByteString      = []byte("air_temp_hum_indoor")       // heap optimization
+)
+
+var (
+	// Field keys for 'air condition indoor' points.
+	ATHIFieldKeys = [][]byte{
+		[]byte("temperature"),
+		[]byte("humidity"),
+		[]byte("battery_voltage"),
+	}
+)
+
+type ATHIMeasurement struct {
+	timestamp     time.Time
+	distributions []Distribution
+}
+
+func NewATHIMeasurement(start time.Time) *ATHIMeasurement {
+	distributions := make([]Distribution, len(ATHIFieldKeys))
+	//temperature
+	distributions[0] = MUDWD(ND(0,1), 15, 28, 15 )
+	//humidity
+	distributions[1] = MUDWD(ND(0,1), 25, 60, 40 )
+	//battery_voltage
+	distributions[2] = MUDWD(ND(1,0.5), 1, 3.2, 3.2 )
+	return &ATHIMeasurement{
+		timestamp:   start,
+		distributions: distributions,
+	}
+}
+
+func (m *ATHIMeasurement) Tick(d time.Duration) {
+	m.timestamp = m.timestamp.Add(d)
+	for i := range m.distributions {
+		m.distributions[i].Advance()
+	}
+}
+
+func (m *ATHIMeasurement) ToPoint(p *Point) {
+	p.SetMeasurementName(ATHIByteString)
+	p.SetTimestamp(&m.timestamp)
+
+	for i := range m.distributions {
+		p.AppendField(ATHIFieldKeys[i], m.distributions[i].Get())
+	}
+}
