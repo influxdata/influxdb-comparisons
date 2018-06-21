ff7d11201a688eaf49cad4fbd9637b3929ac8950
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-06-21 14:19:20 +0200

    Better variable and func names

--- bulk_data_gen/iot/air_temp_hum_indoor.go
+++ bulk_data_gen/iot/air_temp_hum_indoor.go
@@ -6,26 +6,26 @@ import (
 )
 
 var (
-	ATHIByteString      = []byte("air_condition_room")       // heap optimization
+	AirConditionRoomByteString      = []byte("air_condition_room")       // heap optimization
 )
 
 var (
 	// Field keys for 'air condition indoor' points.
-	ATHIFieldKeys = [][]byte{
+	AirConditionRoomFieldKeys = [][]byte{
 		[]byte("temperature"),
 		[]byte("humidity"),
 		[]byte("battery_voltage"),
 	}
 )
 
-type ATHIMeasurement struct {
+type AirConditionRoomMeasurement struct {
 	sensorId 	[]byte
 	timestamp     time.Time
 	distributions []Distribution
 }
 
-func NewATHIMeasurement(start time.Time, id []byte) *ATHIMeasurement {
-	distributions := make([]Distribution, len(ATHIFieldKeys))
+func NewAirConditionRoomMeasurement(start time.Time, id []byte) *AirConditionRoomMeasurement {
+	distributions := make([]Distribution, len(AirConditionRoomFieldKeys))
 	//temperature
 	distributions[0] = MUDWD(ND(0,1), 15, 28, 15 )
 	//humidity
@@ -33,25 +33,25 @@ func NewATHIMeasurement(start time.Time, id []byte) *ATHIMeasurement {
 	//battery_voltage
 	distributions[2] = MUDWD(ND(1,0.5), 1, 3.2, 3.2 )
 
-	return &ATHIMeasurement{
+	return &AirConditionRoomMeasurement{
 		timestamp:   start,
 		distributions: distributions,
 		sensorId: id,
 	}
 }
 
-func (m *ATHIMeasurement) Tick(d time.Duration) {
+func (m *AirConditionRoomMeasurement) Tick(d time.Duration) {
 	m.timestamp = m.timestamp.Add(d)
 	for i := range m.distributions {
 		m.distributions[i].Advance()
 	}
 }
 
-func (m *ATHIMeasurement) ToPoint(p *Point) {
-	p.SetMeasurementName(ATHIByteString)
+func (m *AirConditionRoomMeasurement) ToPoint(p *Point) {
+	p.SetMeasurementName(AirConditionRoomByteString)
 	p.SetTimestamp(&m.timestamp)
 
 	for i := range m.distributions {
-		p.AppendField(ATHIFieldKeys[i], m.distributions[i].Get())
+		p.AppendField(AirConditionRoomFieldKeys[i], m.distributions[i].Get())
 	}
 }
