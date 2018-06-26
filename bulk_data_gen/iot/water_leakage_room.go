4628d474e9c30635c004cca19a9beb6db923dc7c
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-06-26 14:07:38 +0200

    Added missing sensor_id tag

--- bulk_data_gen/iot/water_leakage_room.go
+++ bulk_data_gen/iot/water_leakage_room.go
@@ -49,6 +49,7 @@ func (m *WaterLeakageRoomMeasurement) Tick(d time.Duration) {
 func (m *WaterLeakageRoomMeasurement) ToPoint(p *Point) {
 	p.SetMeasurementName(WaterLeakageRoomByteString)
 	p.SetTimestamp(&m.timestamp)
+	p.AppendTag(SensorHomeTagKeys[0], m.sensorId)
 	p.AppendTag(RoomTagKey, m.roomId)
 	for i := range m.distributions {
 		p.AppendField(WaterLeakageRoomFieldKeys[i], m.distributions[i].Get())
