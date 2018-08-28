b421c21538f21fe15109150a8ca9882928027ebe
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-08-28 16:42:02 +0200

    Added dashboard specific host - grouped into clusters, named meta or data

---
+++ bulk_data_gen/dashboard/host.go
@@ -0,0 +1,95 @@
+package dashboard
+
+import (
+	"fmt"
+	. "github.com/influxdata/influxdb-comparisons/bulk_data_gen/common"
+	"github.com/influxdata/influxdb-comparisons/bulk_data_gen/devops"
+	"math/rand"
+	"time"
+)
+
+const NHostSims = 11
+
+var ClusterSizes = []int{5, 6, 7, 8, 9, 10, 11, 12, 13}
+
+// Type Host models a machine being monitored by Telegraf.
+type Host struct {
+	SimulatedMeasurements []SimulatedMeasurement
+
+	// These are all assigned once, at Host creation:
+	Name, Region, Datacenter, Rack, OS, Arch               []byte
+	ClusterId, Service, ServiceVersion, ServiceEnvironment []byte
+}
+
+func NewHostMeasurements(start time.Time) []SimulatedMeasurement {
+	sm := []SimulatedMeasurement{
+		devops.NewCPUMeasurement(start),
+		devops.NewDiskIOMeasurement(start),
+		devops.NewDiskMeasurement(start),
+		devops.NewKernelMeasurement(start),
+		devops.NewMemMeasurement(start),
+		devops.NewNetMeasurement(start),
+		devops.NewNginxMeasurement(start),
+		devops.NewPostgresqlMeasurement(start),
+		devops.NewRedisMeasurement(start),
+		NewSystemMeasurement(start),
+		NewStatusMeasurement(start),
+	}
+
+	if len(sm) != NHostSims {
+		panic("logic error: incorrect number of measurements")
+	}
+	return sm
+}
+
+var (
+	curentClusterSize int
+	clusterId         int
+	currentHostIndex  int
+)
+
+func NewHost(i int, start time.Time) Host {
+	if curentClusterSize == 0 || currentHostIndex == curentClusterSize {
+		currentHostIndex = 0
+		curentClusterSize = ClusterSizes[rand.Intn(len(ClusterSizes))]
+		clusterId++
+	}
+	var hostname []byte
+	if currentHostIndex < 3 {
+		hostname = []byte(fmt.Sprintf("meta_%d", currentHostIndex+1))
+	} else {
+		hostname = []byte(fmt.Sprintf("data_%d", currentHostIndex-2))
+	}
+	sm := NewHostMeasurements(start)
+
+	region := &devops.Regions[rand.Intn(len(devops.Regions))]
+	rackId := rand.Int63n(devops.MachineRackChoicesPerDatacenter)
+	serviceId := rand.Int63n(devops.MachineServiceChoices)
+	serviceVersionId := rand.Int63n(devops.MachineServiceVersionChoices)
+	serviceEnvironment := RandChoice(devops.MachineServiceEnvironmentChoices)
+
+	h := Host{
+		// Tag Values that are static throughout the life of a Host:
+		Name:               hostname,
+		Region:             []byte(fmt.Sprintf("%s", region.Name)),
+		Datacenter:         RandChoice(region.Datacenters),
+		Rack:               []byte(fmt.Sprintf("%d", rackId)),
+		Arch:               RandChoice(devops.MachineArchChoices),
+		OS:                 RandChoice(devops.MachineOSChoices),
+		Service:            []byte(fmt.Sprintf("%d", serviceId)),
+		ServiceVersion:     []byte(fmt.Sprintf("%d", serviceVersionId)),
+		ServiceEnvironment: serviceEnvironment,
+		ClusterId:          []byte(fmt.Sprintf("%d", clusterId)),
+
+		SimulatedMeasurements: sm,
+	}
+	currentHostIndex++
+	return h
+}
+
+// TickAll advances all Distributions of a Host.
+func (h *Host) TickAll(d time.Duration) {
+	for i := range h.SimulatedMeasurements {
+		h.SimulatedMeasurements[i].Tick(d)
+	}
+}
