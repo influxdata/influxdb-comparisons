f32342fc02b682e288fb3307a526d001bbb85ba5
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-09-04 16:13:32 +0200

    Fixed query error

--- bulk_query_gen/influxdb/influx_dashboard_disk_allocated.go
+++ bulk_query_gen/influxdb/influx_dashboard_disk_allocated.go
@@ -37,7 +37,7 @@ func (d *InfluxDashboardDiskAllocated) Dispatch(i int) bulkQuerygen.Query {
 	clusterId := fmt.Sprintf("%d", rand.Intn(15))
 	var query string
 	//SELECT last("max") from (SELECT max("total")/1073741824 FROM "telegraf"."default"."disk" WHERE time > :dashboardTime: and cluster_id = :Cluster_Id: and host =~ /.data./ GROUP BY time(120s))
-	query = fmt.Sprintf("SELECT last(\"max\") from (SELECT max(\"total\")/1073741824 FROM disk WHERE cluster_id = '%s' and time >= '%s' and time < '%s' hostname =~ /.data./ group by time(120s))", clusterId, interval.StartString(), interval.EndString())
+	query = fmt.Sprintf("SELECT last(\"max\") from (SELECT max(\"total\")/1073741824 FROM disk WHERE cluster_id = '%s' and time >= '%s' and time < '%s' and hostname =~ /.data./ group by time(120s))", clusterId, interval.StartString(), interval.EndString())
 
 	humanLabel := fmt.Sprintf("InfluxDB (%s) Disk Allocated (GB), rand cluster, %s by 120s", d.language.String(), d.queryTimeRange)
 
