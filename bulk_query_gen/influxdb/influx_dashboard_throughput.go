2dfa20864d078892114e5c44cdbb5dcf7bec3748
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-09-12 10:23:05 +0200

    fixed query label

--- bulk_query_gen/influxdb/influx_dashboard_throughput.go
+++ bulk_query_gen/influxdb/influx_dashboard_throughput.go
@@ -37,7 +37,7 @@ func (d *InfluxDashboardThroughput) Dispatch(i int) bulkQuerygen.Query {
 	//SELECT non_negative_derivative(max("pointReqLocal"), 10s) FROM "telegraf"."default"."influxdb_write" WHERE "cluster_id" = :Cluster_Id: AND time > :dashboardTime: GROUP BY time(1m), "host"
 	query = fmt.Sprintf("SELECT non_negative_derivative(max(\"keyspace_hits\"), 10s) FROM redis WHERE cluster_id = '%s' and time >= '%s' and time < '%s' group by time(1m), \"hostname\"", d.GetRandomClusterId(), interval.StartString(), interval.EndString())
 
-	humanLabel := fmt.Sprintf("InfluxDB (%s) Per-Host Point Throughput (Number, %s by 1m", d.language.String(), d.queryTimeRange)
+	humanLabel := fmt.Sprintf("InfluxDB (%s) Per-Host Point Throughput (Number), %s by 1m", d.language.String(), d.queryTimeRange)
 
 	d.getHttpQuery(humanLabel, interval.StartString(), query, q)
 	return q
