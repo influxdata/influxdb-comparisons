0f4df1f237742f03f7674eaed95815003d041059
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-09-12 10:17:07 +0200

    Added string escaping

--- util/report/result.go
+++ util/report/result.go
@@ -101,6 +101,26 @@ func finishReport(c *Collector, p *Point) error {
 	return err
 }
 
+const escapes = "\t\n\f\r ,="
+
+var escaper = strings.NewReplacer(
+	"\t", `\t`,
+	"\n", `\n`,
+	"\f", `\f`,
+	"\r", `\r`,
+	`,`, `\,`,
+	` `, `\ `,
+	`=`, `\=`,
+)
+
+func escape(s string) string {
+	if strings.ContainsAny(s, escapes) {
+		return escaper.Replace(s)
+	} else {
+		return s
+	}
+}
+
 //ReportQueryResult send result from bulk query benchmark to an influxdb according to the given parameters
 func ReportQueryResult(params *QueryReportParams, queryName string, minQueryTime float64, meanQueryTime float64, maxQueryTime float64, totalQueries int64, queryDuration time.Duration) error {
 
@@ -110,14 +130,26 @@ func ReportQueryResult(params *QueryReportParams, queryName string, minQueryTime
 	}
 
 	p.AddTag("burn_in", strconv.Itoa(int(params.BurnIn)))
-	p.AddTag("query_name", queryName)
+	p.AddTag("query_name", escape(queryName))
 
 	p.AddFloat64Field("min_time", minQueryTime)
-	p.AddFloat64Field("min_rate", 1000/minQueryTime)
+	if minQueryTime > 0 {
+		p.AddFloat64Field("min_rate", 1000/minQueryTime)
+	} else {
+		p.AddFloat64Field("min_rate", -1)
+	}
 	p.AddFloat64Field("mean_time", meanQueryTime)
-	p.AddFloat64Field("mean_rate", 1000/meanQueryTime)
+	if meanQueryTime > 0 {
+		p.AddFloat64Field("mean_rate", 1000/meanQueryTime)
+	} else {
+		p.AddFloat64Field("mean_rate", -1)
+	}
 	p.AddFloat64Field("max_time", maxQueryTime)
-	p.AddFloat64Field("max_rate", 1000/maxQueryTime)
+	if maxQueryTime > 0 {
+		p.AddFloat64Field("max_rate", 1000/maxQueryTime)
+	} else {
+		p.AddFloat64Field("max_rate", -1)
+	}
 	p.AddInt64Field("total_items", totalQueries)
 	p.AddFloat64Field("duration", queryDuration.Seconds())
 
