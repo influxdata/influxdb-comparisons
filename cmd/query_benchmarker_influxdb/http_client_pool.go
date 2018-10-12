13ce35a441b8c760aa32e76270c982fe7d165694
Ales Pour <ales.pour@bonitoo.io>
2018-10-12 11:46:39 +0200

    precreated httpclient pool

---
+++ cmd/query_benchmarker_influxdb/http_client_pool.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"sync"
+	"time"
+)
+
+type HTTPClientPool struct {
+	Pool 	*sync.Pool
+	Host 	string
+	Debug 	int
+	Timeout time.Duration
+}
+
+func (p *HTTPClientPool) CachedOrNewHTTPClient() *HTTPClient {
+	return p.Pool.Get().(*HTTPClient)
+}
+
+var ClientsPerHost = 512
+var clientsPools map[string]*HTTPClientPool
+
+func InitPool(urls []string, debug int, timeout time.Duration) {
+	clientsPools = make(map[string]*HTTPClientPool)
+	for i := 0; i < len(urls); i++ {
+		hp := HTTPClientPool{
+			Host: urls[i],
+			Debug: debug,
+			Timeout: timeout,
+		}
+		hp.Pool = &sync.Pool{
+			New: func()interface{} {
+				fmt.Printf("pool for %v depleted, adding new client", hp.Host)
+				return NewHTTPClient(hp.Host, hp.Debug, hp.Timeout)
+			},
+		}
+		for c := 0; c < ClientsPerHost; c++ {
+			c := hp.Pool.New().(*HTTPClient)
+			c.ping()
+			hp.Pool.Put(c)
+		}
+		clientsPools[hp.Host] = &hp
+	}
+}
+
+func (w *HTTPClient) ping() {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	req.Header.SetMethod("GET")
+	req.Header.SetRequestURI(w.HostString + "/ping")
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+	_ = w.client.Do(req, resp)
+}
+
+func CachedOrNewHTTPClient(host string, debug int, timeout time.Duration) *HTTPClient {
+	return clientsPools[host].CachedOrNewHTTPClient()
+}
+
+
