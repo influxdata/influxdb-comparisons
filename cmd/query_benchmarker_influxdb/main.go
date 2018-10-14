6da60d8155d57b1c3ba890c62156c29a8c127582
Vlasta Hajek <vlastimil.hajek@gmail.com>
2018-10-14 20:05:11 +0200

    skipping if block reentering

--- cmd/query_benchmarker_influxdb/main.go
+++ cmd/query_benchmarker_influxdb/main.go
@@ -280,7 +280,7 @@ loop:
 				}
 			}
 		case <-responseTicker.C:
-			if responseTimeLimit.Nanoseconds() > 0 && responseTimeLimit.Nanoseconds() < int64(movingAverageStat.Avg()*1e6) && statMapping[allQueriesLabel].Count > 1000 {
+			if !responseTimeLimitReached && responseTimeLimit.Nanoseconds() > 0 && responseTimeLimit.Nanoseconds() < int64(movingAverageStat.Avg()*1e6) && statMapping[allQueriesLabel].Count > 1000 {
 				responseTimeLimitReached = true
 				fmt.Printf("Mean response time is above threshold: %.2fms > %.2fms\n", movingAverageStat.Avg(), float64(responseTimeLimit.Nanoseconds())/1e6)
 				scanClose <- 1
