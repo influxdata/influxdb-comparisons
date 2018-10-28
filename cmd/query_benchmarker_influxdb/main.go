0759096ae3f97df7c9a65ba7cc08b52bd43b17ff
Ales Pour <ales.pour@bonitoo.io>
2018-10-28 16:42:09 +0100

    fixed condition

--- cmd/query_benchmarker_influxdb/main.go
+++ cmd/query_benchmarker_influxdb/main.go
@@ -654,7 +654,7 @@ func processStats() {
 
 		i++
 
-		if lastRefresh.Second() == 0 || now.Sub(lastRefresh).Seconds() > 1 {
+		if lastRefresh.Second() == 0 || now.Sub(lastRefresh).Seconds() >= 1 {
 			movingAverageStat.UpdateAvg(now, workers)
 			lastRefresh = now
 		}
