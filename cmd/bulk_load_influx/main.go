28bc2b5e5db39a04841354352a900f51c6febe8c
Ales Pour <ales.pour@bonitoo.io>
2018-11-06 16:08:08 +0100

    wait longer more backoff responses is received

--- cmd/bulk_load_influx/main.go
+++ cmd/bulk_load_influx/main.go
@@ -565,6 +565,7 @@ func processBatches(w *HTTPWriter, backoffSrc chan bool, backoffDst chan struct{
 		// Write the batch: try until backoff is not needed.
 		if doLoad {
 			var err error
+			var extraSleep time.Duration = 0
 			for {
 				if useGzip {
 					compressedBatch := bufPool.Get().(*bytes.Buffer)
@@ -594,7 +595,8 @@ func processBatches(w *HTTPWriter, backoffSrc chan bool, backoffDst chan struct{
 						p.AddBoolField("backoff", true)
 						telemetrySink <- p
 					}
-					time.Sleep(backoff)
+					time.Sleep(backoff + extraSleep)
+					extraSleep += backoff
 				} else {
 					backoffSrc <- false
 					break
