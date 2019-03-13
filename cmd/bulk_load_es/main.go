ef635ac7dbc81b21a92cac9566394f82aa807b05
vlastahajek <vlastimil.hajek@bonitoo.io>
2019-03-13 18:01:11 +0100

    Elastic bulk load adapted to common query benchmarker interface

--- cmd/bulk_load_es/main.go
+++ cmd/bulk_load_es/main.go
@@ -11,6 +11,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/influxdata/influxdb-comparisons/bulk_data_gen/common"
+	"github.com/influxdata/influxdb-comparisons/bulk_load"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -24,46 +25,6 @@ import (
 	"github.com/influxdata/influxdb-comparisons/util/report"
 	"github.com/valyala/fasthttp"
 	"strconv"
-)
-
-// Program option vars:
-var (
-	csvDaemonUrls      string
-	daemonUrls         []string
-	refreshEachBatch   bool
-	workers            int
-	batchSize          int
-	itemLimit          int64
-	indexTemplateName  string
-	useGzip            bool
-	doLoad             bool
-	doDBCreate         bool
-	numberOfReplicas   uint
-	numberOfShards     uint
-	telemetryHost      string
-	telemetryStderr    bool
-	telemetryBatchSize uint64
-	telemetryTagsCSV   string
-	telemetryBasicAuth string
-	reportDatabase     string
-	reportHost         string
-	reportUser         string
-	reportPassword     string
-	reportTagsCSV      string
-)
-
-// Global vars
-var (
-	bufPool             sync.Pool
-	batchChan           chan *bytes.Buffer
-	inputDone           chan struct{}
-	workersGroup        sync.WaitGroup
-	telemetryChanPoints chan *report.Point
-	telemetryChanDone   chan struct{}
-	telemetryHostname   string
-	telemetryTags       [][2]string
-	reportTags          [][2]string
-	reportHostname      string
 )
 
 // Args parsing vars
@@ -228,207 +189,174 @@ var aggregationTemplate6x = []byte(`
 }
 `)
 
-// Parse args:
-func init() {
-	flag.StringVar(&csvDaemonUrls, "urls", "http://localhost:9200", "ElasticSearch URLs, comma-separated. Will be used in a round-robin fashion.")
-	flag.BoolVar(&refreshEachBatch, "refresh", true, "Whether each batch is immediately indexed.")
-	flag.Int64Var(&itemLimit, "item-limit", -1, "Number of items to read from stdin before quitting. (2 lines of input = 1 item)")
+type ElasticBulkLoad struct {
+	// Program option vars:
+	csvDaemonUrls     string
+	daemonUrls        []string
+	refreshEachBatch  bool
+	indexTemplateName string
+	useGzip           bool
+	numberOfReplicas  uint
+	numberOfShards    uint
+	// Global vars
+	bufPool      sync.Pool
+	batchChan    chan *bytes.Buffer
+	inputDone    chan struct{}
+	scanFinished bool
+}
 
-	flag.IntVar(&batchSize, "batch-size", 5000, "Batch size (input items).")
-	flag.IntVar(&workers, "workers", 1, "Number of parallel requests to make.")
+func (l *ElasticBulkLoad) Init() {
+	flag.StringVar(&l.csvDaemonUrls, "urls", "http://localhost:9200", "ElasticSearch URLs, comma-separated. Will be used in a round-robin fashion.")
+	flag.BoolVar(&l.refreshEachBatch, "refresh", true, "Whether each batch is immediately indexed.")
 
-	flag.StringVar(&indexTemplateName, "index-template", "default", "ElasticSearch index template to use (choices: default, aggregation).")
+	flag.StringVar(&l.indexTemplateName, "index-template", "default", "ElasticSearch index template to use (choices: default, aggregation).")
 
-	flag.BoolVar(&useGzip, "gzip", true, "Whether to gzip encode requests (default true).")
+	flag.BoolVar(&l.useGzip, "gzip", true, "Whether to gzip encode requests (default true).")
 
-	flag.BoolVar(&doLoad, "do-load", true, "Whether to write data. Set this flag to false to check input read speed.")
-	flag.BoolVar(&doDBCreate, "do-db-create", true, "Whether to create the database.")
+	flag.UintVar(&l.numberOfReplicas, "number-of-replicas", 0, "Number of ES replicas (note: replicas == replication_factor - 1). Zero replicas means RF of 1.")
+	flag.UintVar(&l.numberOfShards, "number-of-shards", 1, "Number of ES shards. Typically you will set this to the number of nodes in the cluster.")
 
-	flag.UintVar(&numberOfReplicas, "number-of-replicas", 0, "Number of ES replicas (note: replicas == replication_factor - 1). Zero replicas means RF of 1.")
-	flag.UintVar(&numberOfShards, "number-of-shards", 1, "Number of ES shards. Typically you will set this to the number of nodes in the cluster.")
+}
 
-	flag.StringVar(&telemetryHost, "telemetry-host", "", "InfluxDB host to write telegraf telemetry to (optional).")
-	flag.BoolVar(&telemetryStderr, "telemetry-stderr", false, "Whether to write telemetry also to stderr.")
-	flag.Uint64Var(&telemetryBatchSize, "telemetry-batch-size", 100, "Telemetry batch size (lines).")
-	flag.StringVar(&telemetryBasicAuth, "telemetry-basic-auth", "", "basic auth (username:password) for telemetry.")
-	flag.StringVar(&telemetryTagsCSV, "telemetry-tags", "", "Tag(s) for telemetry. Format: key0:val0,key1:val1,...")
-
-	flag.StringVar(&reportDatabase, "report-database", "database_benchmarks", "Database name where to store result metrics")
-	flag.StringVar(&reportHost, "report-host", "", "Host to send result metrics")
-	flag.StringVar(&reportUser, "report-user", "", "User for host to send result metrics")
-	flag.StringVar(&reportPassword, "report-password", "", "User password for Host to send result metrics")
-	flag.StringVar(&reportTagsCSV, "report-tags", "", "Comma separated k:v tags to send  alongside result metrics")
-
-	flag.Parse()
-
-	daemonUrls = strings.Split(csvDaemonUrls, ",")
-	if len(daemonUrls) == 0 {
+func (l *ElasticBulkLoad) Validate() {
+	l.daemonUrls = strings.Split(l.csvDaemonUrls, ",")
+	if len(l.daemonUrls) == 0 {
 		log.Fatal("missing 'urls' flag")
 	}
-	fmt.Printf("daemon URLs: %v\n", daemonUrls)
+	fmt.Printf("daemon URLs: %v\n", l.daemonUrls)
 
-	if telemetryHost != "" {
-		fmt.Printf("telemetry destination: %v\n", telemetryHost)
-		if telemetryBatchSize == 0 {
-			panic("invalid telemetryBatchSize")
-		}
-		var err error
-		telemetryHostname, err = os.Hostname()
-		if err != nil {
-			log.Fatalf("os.Hostname() error: %s", err.Error())
-		}
-		fmt.Printf("src addr for telemetry: %v\n", telemetryHostname)
-
-		if telemetryTagsCSV != "" {
-			pairs := strings.Split(telemetryTagsCSV, ",")
-			for _, pair := range pairs {
-				fields := strings.SplitN(pair, ":", 2)
-				tagpair := [2]string{fields[0], fields[1]}
-				telemetryTags = append(telemetryTags, tagpair)
-			}
-		}
-		fmt.Printf("telemetry tags: %v\n", telemetryTags)
-	}
-
-	if reportHost != "" {
-		fmt.Printf("results report destination: %v\n", reportHost)
-		fmt.Printf("results report database: %v\n", reportDatabase)
-
-		var err error
-		reportHostname, err = os.Hostname()
-		if err != nil {
-			log.Fatalf("os.Hostname() error: %s", err.Error())
-		}
-		fmt.Printf("hostname for results report: %v\n", reportHostname)
-
-		if reportTagsCSV != "" {
-			pairs := strings.Split(reportTagsCSV, ",")
-			for _, pair := range pairs {
-				fields := strings.SplitN(pair, ":", 2)
-				tagpair := [2]string{fields[0], fields[1]}
-				reportTags = append(reportTags, tagpair)
-			}
-		}
-		fmt.Printf("results report tags: %v\n", reportTags)
-	}
-
-	if _, ok := indexTemplateChoices[indexTemplateName]; !ok {
+	if _, ok := indexTemplateChoices[l.indexTemplateName]; !ok {
 		log.Fatalf("invalid index template type")
 	}
 }
 
-func main() {
-	if doLoad {
-		v, err := checkServer(daemonUrls[0])
+func (l *ElasticBulkLoad) CreateDb() {
+	v, err := checkServer(l.daemonUrls[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if bulk_load.Runner.DoDBCreate {
+		// check that there are no pre-existing index templates:
+		existingIndexTemplates, err := listIndexTemplates(l.daemonUrls[0])
 		if err != nil {
 			log.Fatal(err)
 		}
-		if doDBCreate {
-			// check that there are no pre-existing index templates:
-			existingIndexTemplates, err := listIndexTemplates(daemonUrls[0])
-			if err != nil {
-				log.Fatal(err)
-			}
 
-			if len(existingIndexTemplates) > 0 {
-				log.Println("There are index templates already in the data store. If you know what you are doing, clear them first with a command like:\ncurl -XDELETE 'http://localhost:9200/_template/*'")
-			}
+		if len(existingIndexTemplates) > 0 {
+			log.Println("There are index templates already in the data store. If you know what you are doing, clear them first with a command like:\ncurl -XDELETE 'http://localhost:9200/_template/*'")
+		}
 
-			// check that there are no pre-existing indices:
-			existingIndices, err := listIndices(daemonUrls[0])
-			if err != nil {
-				log.Fatal(err)
-			}
+		// check that there are no pre-existing indices:
+		existingIndices, err := listIndices(l.daemonUrls[0])
+		if err != nil {
+			log.Fatal(err)
+		}
 
-			if len(existingIndices) > 0 {
-				log.Println("There are indices already in the data store. If you know what you are doing, clear them first with a command like:\ncurl -XDELETE 'http://localhost:9200/_all'")
-			}
+		if len(existingIndices) > 0 {
+			log.Println("There are indices already in the data store. If you know what you are doing, clear them first with a command like:\ncurl -XDELETE 'http://localhost:9200/_all'")
+		}
 
-			// create the index template:
-			indexTemplate := indexTemplateChoices[indexTemplateName]
-			err = createESTemplate(daemonUrls[0], "measurements_template", indexTemplate[v], numberOfReplicas, numberOfShards)
-			if err != nil {
-				log.Fatal(err)
-			}
+		// create the index template:
+		indexTemplate := indexTemplateChoices[l.indexTemplateName]
+		err = createESTemplate(l.daemonUrls[0], "measurements_template", indexTemplate[v], l.numberOfReplicas, l.numberOfShards)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
-	bufPool = sync.Pool{
+}
+
+func (l *ElasticBulkLoad) PrepareWorkers() {
+	l.bufPool = sync.Pool{
 		New: func() interface{} {
 			return bytes.NewBuffer(make([]byte, 0, 4*1024*1024))
 		},
 	}
 
-	batchChan = make(chan *bytes.Buffer, workers)
-	inputDone = make(chan struct{})
+	l.batchChan = make(chan *bytes.Buffer, bulk_load.Runner.Workers)
+	l.inputDone = make(chan struct{})
+}
 
-	if telemetryHost != "" {
-		telemetryCollector := report.NewCollector(telemetryHost, "telegraf", reportUser, reportPassword)
-		telemetryChanPoints, telemetryChanDone = report.TelemetryRunAsync(telemetryCollector, telemetryBatchSize, telemetryStderr, 0)
+func (l *ElasticBulkLoad) GetBatchProcessor() bulk_load.BatchProcessor {
+	return l
+}
+
+func (l *ElasticBulkLoad) GetScanner() bulk_load.Scanner {
+	return l
+}
+
+func (l *ElasticBulkLoad) SyncEnd() {
+	<-l.inputDone
+	close(l.batchChan)
+}
+
+func (l *ElasticBulkLoad) CleanUp() {
+
+}
+
+func (l *ElasticBulkLoad) UpdateReport(params *report.LoadReportParams) (reportTags [][2]string, extraVals []report.ExtraVal) {
+
+	reportTags = [][2]string{{"replicas", strconv.Itoa(int(l.numberOfReplicas))}}
+	reportTags = append(reportTags, [2]string{"shards", strconv.Itoa(int(l.numberOfShards))})
+	reportTags = append(reportTags, [2]string{"index-template", l.indexTemplateName})
+
+	params.DBType = "ElasticSearch"
+	params.DestinationUrl = l.csvDaemonUrls
+	params.IsGzip = l.useGzip
+
+	return
+}
+
+func (l *ElasticBulkLoad) PrepareProcess(i int) {
+
+}
+
+func (l *ElasticBulkLoad) RunProcess(i int, waitGroup *sync.WaitGroup, telemetryPoints chan *report.Point, reportTags [][2]string) error {
+	daemonUrl := l.daemonUrls[i%len(l.daemonUrls)]
+
+	cfg := HTTPWriterConfig{
+		Host: daemonUrl,
 	}
+	return l.processBatches(NewHTTPWriter(cfg, l.refreshEachBatch), waitGroup, telemetryPoints, fmt.Sprintf("%d", i))
+}
 
-	for i := 0; i < workers; i++ {
-		daemonUrl := daemonUrls[i%len(daemonUrls)]
-		workersGroup.Add(1)
-		cfg := HTTPWriterConfig{
-			Host: daemonUrl,
-		}
-		go processBatches(NewHTTPWriter(cfg, refreshEachBatch), telemetryChanPoints, fmt.Sprintf("%d", i))
+func (l *ElasticBulkLoad) AfterRunProcess(i int) {
+
+}
+
+func (l *ElasticBulkLoad) EmptyBatchChanel() {
+	for range l.batchChan {
+		//read out remaining batches
 	}
+}
 
-	start := time.Now()
-	itemsRead, bytesRead, valuesRead := scan(batchSize)
+func (l *ElasticBulkLoad) IsScanFinished() bool {
+	return l.scanFinished
+}
 
-	<-inputDone
-	close(batchChan)
-	workersGroup.Wait()
-	end := time.Now()
-	took := end.Sub(start)
-	itemsRate := float64(itemsRead) / float64(took.Seconds())
-	bytesRate := float64(bytesRead) / float64(took.Seconds())
-	valuesRate := float64(valuesRead) / float64(took.Seconds())
+var load = &ElasticBulkLoad{}
 
-	if telemetryHost != "" {
-		close(telemetryChanPoints)
-		<-telemetryChanDone
-	}
+// Parse args:
+func init() {
+	bulk_load.Runner.Init(5000)
+	load.Init()
 
-	fmt.Printf("loaded %d items in %fsec with %d workers (mean point rate %f items/sec, mean value rate %f/s, %.2fMB/sec from stdin)\n", itemsRead, took.Seconds(), workers, itemsRate, valuesRate, bytesRate/(1<<20))
+	flag.Parse()
 
-	if reportHost != "" {
-		//append db specific tags to custom tags
-		reportTags = append(reportTags, [2]string{"replicas", strconv.Itoa(int(numberOfReplicas))})
-		reportTags = append(reportTags, [2]string{"shards", strconv.Itoa(int(numberOfShards))})
-		reportTags = append(reportTags, [2]string{"index-template", indexTemplateName})
+	bulk_load.Runner.Validate()
+	load.Validate()
 
-		reportParams := &report.LoadReportParams{
-			ReportParams: report.ReportParams{
-				DBType:             "ElasticSearch",
-				ReportDatabaseName: reportDatabase,
-				ReportHost:         reportHost,
-				ReportUser:         reportUser,
-				ReportPassword:     reportPassword,
-				ReportTags:         reportTags,
-				Hostname:           reportHostname,
-				DestinationUrl:     csvDaemonUrls,
-				Workers:            workers,
-				ItemLimit:          int(itemLimit),
-			},
-			IsGzip:    useGzip,
-			BatchSize: batchSize,
-		}
-		err := report.ReportLoadResult(reportParams, itemsRead, valuesRate, bytesRate, took)
+}
 
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+func main() {
+	bulk_load.Runner.Run(load)
 }
 
 // scan reads items from stdin. It expects input in the ElasticSearch bulk
 // format: two line pairs, the first line being an 'action' and the second line
 // being the payload. (2 lines = 1 item)
-func scan(itemsPerBatch int) (int64, int64, int64) {
-	buf := bufPool.Get().(*bytes.Buffer)
+func (l *ElasticBulkLoad) RunScanner(syncChanDone chan int) (int64, int64, int64) {
+	l.scanFinished = false
+	buf := l.bufPool.Get().(*bytes.Buffer)
 
 	var linesRead int64
 	var err error
@@ -438,6 +366,11 @@ func scan(itemsPerBatch int) (int64, int64, int64) {
 	var itemsThisBatch int
 	scanner := bufio.NewScanner(os.Stdin)
 
+	var deadline time.Time
+	if bulk_load.Runner.TimeLimit > 0 {
+		deadline = time.Now().Add(bulk_load.Runner.TimeLimit)
+	}
+outer:
 	for scanner.Scan() {
 
 		totalPoints, totalValues, err = common.CheckTotalValues(scanner.Text())
@@ -459,17 +392,26 @@ func scan(itemsPerBatch int) (int64, int64, int64) {
 			itemsThisBatch++
 		}
 
-		hitLimit := itemLimit >= 0 && itemsRead >= itemLimit
+		hitLimit := bulk_load.Runner.ItemLimit >= 0 && itemsRead >= bulk_load.Runner.ItemLimit
 
-		if itemsThisBatch == itemsPerBatch || hitLimit {
+		if itemsThisBatch == bulk_load.Runner.BatchSize || hitLimit {
 			bytesRead += int64(buf.Len())
-			batchChan <- buf
-			buf = bufPool.Get().(*bytes.Buffer)
+			l.batchChan <- buf
+			buf = l.bufPool.Get().(*bytes.Buffer)
 			itemsThisBatch = 0
+			if bulk_load.Runner.TimeLimit > 0 && time.Now().After(deadline) {
+				bulk_load.Runner.SetPrematureEnd("Timeout elapsed")
+				break outer
+			}
 		}
 
 		if hitLimit {
 			break
+		}
+		select {
+		case <-syncChanDone:
+			break outer
+		default:
 		}
 	}
 
@@ -479,29 +421,35 @@ func scan(itemsPerBatch int) (int64, int64, int64) {
 
 	// Finished reading input, make sure last batch goes out.
 	if itemsThisBatch > 0 {
-		batchChan <- buf
+		l.batchChan <- buf
 	}
 
 	// Closing inputDone signals to the application that we've read everything and can now shut down.
-	close(inputDone)
+	close(l.inputDone)
 
 	// The ES bulk format uses 2 lines per item:
 	if linesRead%2 != 0 {
 		log.Fatalf("the number of lines read was not a multiple of 2, which indicates a bad bulk format for Elastic")
 	}
 	if itemsRead != totalPoints { // totalPoints is unknown (0) when exiting prematurely due to time limit
-		log.Fatalf("Incorrent number of read points: %d, expected: %d:", itemsRead, totalPoints)
+		if !bulk_load.Runner.HasEndedPrematurely() {
+			log.Fatalf("Incorrent number of read items: %d, expected: %d:", itemsRead, totalPoints)
+		} else {
+			totalValues = int64(float64(itemsRead) * bulk_load.ValuesPerMeasurement) // needed for statistics summary
+		}
 	}
 
+	l.scanFinished = true
 	return itemsRead, bytesRead, totalValues
 }
 
 // processBatches reads byte buffers from batchChan and writes them to the target server, while tracking stats on the write.
-func processBatches(w *HTTPWriter, telemetrySink chan *report.Point, telemetryWorkerLabel string) {
+func (l *ElasticBulkLoad) processBatches(w *HTTPWriter, workersGroup *sync.WaitGroup, telemetrySink chan *report.Point, telemetryWorkerLabel string) error {
 	var batchesSeen int64
-	for batch := range batchChan {
+	var rerr error
+	for batch := range l.batchChan {
 		batchesSeen++
-		if !doLoad {
+		if !bulk_load.Runner.DoLoad {
 			continue
 		}
 
@@ -509,41 +457,43 @@ func processBatches(w *HTTPWriter, telemetrySink chan *report.Point, telemetryWo
 		var bodySize int
 
 		// Write the batch.
-		if useGzip {
-			compressedBatch := bufPool.Get().(*bytes.Buffer)
+		if l.useGzip {
+			compressedBatch := l.bufPool.Get().(*bytes.Buffer)
 			fasthttp.WriteGzip(compressedBatch, batch.Bytes())
 			bodySize = len(compressedBatch.Bytes())
 			_, err = w.WriteLineProtocol(compressedBatch.Bytes(), true)
 			// Return the compressed batch buffer to the pool.
 			compressedBatch.Reset()
-			bufPool.Put(compressedBatch)
+			l.bufPool.Put(compressedBatch)
 		} else {
 			bodySize = len(batch.Bytes())
 			_, err = w.WriteLineProtocol(batch.Bytes(), false)
 		}
 
 		if err != nil {
-			log.Fatalf("Error writing: %s\n", err.Error())
+			rerr = fmt.Errorf("Error writing: %s\n", err.Error())
+			break
 		}
 
 		// Return the batch buffer to the pool.
 		batch.Reset()
-		bufPool.Put(batch)
+		l.bufPool.Put(batch)
 
 		// Report telemetry, if applicable:
 		if telemetrySink != nil {
 			p := report.GetPointFromGlobalPool()
 			p.Init("benchmark_write", time.Now().UnixNano())
-			p.AddTag("src_addr", telemetryHostname)
+			p.AddTag("src_addr", telemetryWorkerLabel)
 			p.AddTag("dst_addr", w.c.Host)
 			p.AddTag("worker_id", telemetryWorkerLabel)
 			p.AddInt64Field("worker_req_num", batchesSeen)
-			p.AddBoolField("gzip", useGzip)
+			p.AddBoolField("gzip", l.useGzip)
 			p.AddInt64Field("body_bytes", int64(bodySize))
 			telemetrySink <- p
 		}
 	}
 	workersGroup.Done()
+	return rerr
 }
 
 // createESTemplate uses a Go text/template to create an ElasticSearch index
@@ -592,37 +542,6 @@ func createESTemplate(daemonUrl, indexTemplateName string, indexTemplateBodyTemp
 	}
 	return nil
 }
-
-//func createDb(daemon_url, dbname string) error {
-//	u, err := url.Parse(daemon_url)
-//	if err != nil {
-//		return err
-//	}
-//
-//	// serialize params the right way:
-//	u.Path = "query"
-//	v := u.Query()
-//	v.Set("q", fmt.Sprintf("CREATE DATABASE %s", dbname))
-//	u.RawQuery = v.Encode()
-//
-//	req, err := http.NewRequest("GET", u.String(), nil)
-//	if err != nil {
-//		return err
-//	}
-//
-//	client := &http.Client{}
-//	resp, err := client.Do(req)
-//	if err != nil {
-//		return err
-//	}
-//	defer resp.Body.Close()
-//	// does the body need to be read into the void?
-//
-//	if resp.StatusCode != 200 {
-//		return fmt.Errorf("bad db create")
-//	}
-//	return nil
-//}
 
 // listIndexTemplates lists the existing index templates in ElasticSearch.
 func listIndexTemplates(daemonUrl string) (map[string]interface{}, error) {
